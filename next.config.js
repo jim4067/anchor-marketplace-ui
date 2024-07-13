8a769db6f6ad2b70b8fe046758deefb05c573868
Jimii <jimmyimpulse2@gmail.com>
2024-07-13 10:08:33 +0300

    feat: fix failing build(pino-pretty)

--- next.config.js
+++ next.config.js
@@ -8,7 +8,12 @@ const nextConfig = {
             },
         ],
     },
-
+    reactStrictMode: true,
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false, net: false, tls: false };
+        config.externals.push('pino-pretty', 'encoding');
+        return config;
+    },
 };
 
 module.exports = nextConfig;
